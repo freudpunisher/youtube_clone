d2f5a4a7a7eaf5c396bd6fe3bd9dd96fea106215
freudpunisher <punishergte@gmail.com>
2023-03-01 15:29:01 +0200

    done with sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import {Stack,Typography,Box} from '@mui/material';
+import SideBar from './SideBar';
+
 
 function Feed() {
   return (
-    <div>Feed</div>
+    <Stack sx={{flexDirection:{
+      sx:'column',md:'row'
+    }}}>
+      <Box sx={{height:{sx:'auto',md:'92vh'},borderRight:'1px solid #3d3d3d',px:{sx:0,md:2}}}>
+<SideBar/>
+      <Typography className='copyright' sx={{color:'#fff',mt:1.5}}>
+        copyright 2023 punisher
+      </Typography>
+      </Box>
+
+
+
+      </Stack>
   )
 }
 
